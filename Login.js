98339d0654168d19a4f81c578e1351a020835acc
tulasikumari <kumaritulasi97@gmail.com>
2022-12-23 17:21:43 +0545

    search

--- Login.js
+++ Login.js
@@ -1,18 +1,18 @@
-function submit()
-{
-    let user=document.getElementById("user").value;
-    let pws=document.getElementById("pws").value;
-    let logged=document.getElementById("loggedin");
-// var user=document.getElementById("user").nodeValue;
-// var pws=document.getElementById("pws").nodeValue;
-    if(username=="admin" && password=="pass" ){
-        alert("login succesfull");
-    }
-    else{
-        alert("login unsuccessfull")
-    }
+// function submit()
+// {
+//     let user=document.getElementById("user").value;
+//     let pws=document.getElementById("pws").value;
+//     let logged=document.getElementById("loggedin");
+// // var user=document.getElementById("user").nodeValue;
+// // var pws=document.getElementById("pws").nodeValue;
+//     if(username=="admin" && password=="pass" ){
+//         alert("login succesfull");
+//     }
+//     else{
+//         alert("login unsuccessfull")
+//     }
     
 
-    alert("Incorrect UserID or Password")
-}
+//     alert("Incorrect UserID or Password")
+// }
 
