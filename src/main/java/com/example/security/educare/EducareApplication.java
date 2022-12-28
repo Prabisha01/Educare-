d99c4f3a7cea6b8e5abf93d42019a6dd360e99a7
Prabisha01 <prabishakhadka2000@gmail.com>
2022-12-28 20:21:03 +0545

    database

---
+++ src/main/java/com/example/security/educare/EducareApplication.java
@@ -0,0 +1,13 @@
+package com.example.security.educare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducareApplication.class, args);
+    }
+
+}
