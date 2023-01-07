b07f0570cb70633cb9a6c64285e7160043b4e41e
Prabisha01 <prabishakhadka2000@gmail.com>
2023-01-07 19:06:43 +0545

    updatedpage

--- src/main/resources/static/javascript/Login.js
+++ src/main/resources/static/javascript/Login.js
@@ -1,13 +1,23 @@
-function validate()
-{
-var username=document.getElementById("user").value;
-var password=document.getElementById("pws").value;
-    if(username=="admin" && password=="admin" ){
-      window.location.assign("homepage.html");
-        alert("login succesfull");
-      
+
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+    var username = document.getElementById("user").value;
+    var password = document.getElementById("pws").value;
+    if ( username == "admin" && password == "admin"){
+        alert ("Login successfully");
+        window.location = "homepage.html"; // Redirecting to other page.
+        return false;
     }
     else{
-        alert("Incorrect UserID or Password");
+        attempt --;// Decrementing by one.
+        alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+        if( attempt == 0){
+            document.getElementById("user").disabled = true;
+            document.getElementById("pws").disabled = true;
+            document.getElementById("b1").disabled = true;
+            return false;
+        }
     }
 }
