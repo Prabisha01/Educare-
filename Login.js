08e2c0453272fa473c8eb175a5706cb4f207d3db
tulasikumari <kumaritulasi97@gmail.com>
2023-01-07 14:58:13 +0545

    update

--- Login.js
+++ Login.js
@@ -1,18 +1,23 @@
-// function submit()
-// {
-//     let user=document.getElementById("user").value;
-//     let pws=document.getElementById("pws").value;
-//     let logged=document.getElementById("loggedin");
-// // var user=document.getElementById("user").nodeValue;
-// // var pws=document.getElementById("pws").nodeValue;
-//     if(username=="admin" && password=="pass" ){
-//         alert("login succesfull");
-//     }
-//     else{
-//         alert("login unsuccessfull")
-//     }
-    
 
-//     alert("Incorrect UserID or Password")
-// }
-
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("user").value;
+var password = document.getElementById("pws").value;
+if ( username == "admin" && password == "admin"){
+alert ("Login successfully");
+window.location = "homepage.html"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("user").disabled = true;
+document.getElementById("pws").disabled = true;
+document.getElementById("b1").disabled = true;
+return false;
+}
+}
+}
