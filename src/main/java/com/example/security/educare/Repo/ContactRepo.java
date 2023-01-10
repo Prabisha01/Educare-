f799b87beda09bb1552997e893f7e8a5a9946f4b
Prabisha01 <prabishakhadka2000@gmail.com>
2023-01-10 08:17:32 +0545

    updated

--- src/main/java/com/example/security/educare/Repo/ContactRepo.java
+++ src/main/java/com/example/security/educare/Repo/ContactRepo.java
@@ -1,10 +1,9 @@
 package com.example.security.educare.Repo;
 
-import com.example.security.educare.Entity.User;
-import jakarta.persistence.criteria.CriteriaBuilder;
+import com.example.security.educare.Entity.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepo extends JpaRepository<User, Integer> {
+public interface ContactRepo extends JpaRepository<Contact, Integer> {
 }
---
+++ src/main/java/com/example/security/educare/Services/UserService.java
@@ -0,0 +1,13 @@
+package com.example.security.educare.Services;
+
+
+import com.example.security.educare.UserPojo.ContactPojo;
+import com.example.security.educare.Entity.Contact;
+
+
+
+
+
+public interface UserService { String sendMsg(ContactPojo contactPojo);
+
+}
