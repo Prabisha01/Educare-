cb60aba8a221e16b75f9e6306e8935bab7bf7874
Aakash-900 <aakashchdhry96@gmail.com>
2023-01-17 10:43:35 +0545

    update

--- src/main/java/com/example/security/educare/Repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.security.educare.Repo;
-
-import com.example.security.educare.Entity.User;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-}
