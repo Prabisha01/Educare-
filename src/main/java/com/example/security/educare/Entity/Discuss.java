91e0c2da736372ef6b0131f909b98afe117c02f2
NAWINRAJM <94778263+NAWINRAJM@users.noreply.github.com>
2023-01-27 21:52:56 +0545

    discussion entity

---
+++ src/main/java/com/example/security/educare/Entity/Discuss.java
@@ -0,0 +1,23 @@
+package com.system.springboot4.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "discuss")
+public class Discuss {
+    @Id
+    @SequenceGenerator(name = "cms_user_seq_gen", sequenceName = "cms_user_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "cms_user_seq_gen", strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String message;
+
+}
