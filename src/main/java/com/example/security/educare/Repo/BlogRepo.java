aaba0e0b07d806ee62093e68fc38d4b335c7cec9
Prabisha01 <prabishakhadka2000@gmail.com>
2023-01-28 20:45:16 +0545

    updated

--- src/main/java/com/example/security/educare/Repo/BlogRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.security.educare.Repo;
-
-import com.example.security.educare.Entity.Blog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface BlogRepo extends JpaRepository<Blog, Integer> {
-}
--- src/main/java/com/example/security/educare/Repo/ContactRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.security.educare.Repo;
-
-import com.example.security.educare.Entity.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactRepo extends JpaRepository<Contact, Integer> {
-}
