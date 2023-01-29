fd815574860d054207e2ca13c4f289c0c232b441
rabi <wrabwagle123@gmail.com>
2023-01-29 14:52:20 +0545

    all

--- src/main/java/com/example/security/educare/Pojo/discuss.java
+++ src/main/java/com/example/security/educare/Pojo/discuss.java
@@ -1,23 +1,23 @@
-package com.system.springboot4.pojo;
-import com.system.springboot4.entity.Discuss;
-import com.system.springboot4.entity.User;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-// Pojo backend ko validation ko laagi ekdamai important
-
-@Getter
-@Setter
-@NoArgsConstructor // dynamic banaunw duitai pass gareko
-@AllArgsConstructor
-
-public class discuss {
-    private int id;
-    private String message;
-    public discuss(Discuss discuss1){
-        this.id=discuss1.getId();
-        this.message=discuss1.getMessage();
-    }
-}
+//package com.system.springboot4.pojo;
+//import com.system.springboot4.entity.Discuss;
+//import com.system.springboot4.entity.User;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//// Pojo backend ko validation ko laagi ekdamai important
+//
+//@Getter
+//@Setter
+//@NoArgsConstructor // dynamic banaunw duitai pass gareko
+//@AllArgsConstructor
+//
+//public class discuss {
+//    private int id;
+//    private String message;
+//    public discuss(Discuss discuss1){
+//        this.id=discuss1.getId();
+//        this.message=discuss1.getMessage();
+//    }
+//}
