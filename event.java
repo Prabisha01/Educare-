224b7abfef1da9c7536e715c2ffba2912f8c1096
NAWINRAJM <94778263+NAWINRAJM@users.noreply.github.com>
2023-02-23 14:17:35 +0545

    Update event.java

--- event.java
+++ event.java
@@ -1,4 +1,5 @@
-package com.system.springboot4.entity;
+package com.example.security.educare.Entity;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,7 +16,7 @@ public class event {
     @Id
     @SequenceGenerator(name = "cms_user_seq_gen", sequenceName = "cms_user_id_seq", allocationSize = 1)
     @GeneratedValue(generator = "cms_user_seq_gen", strategy = GenerationType.SEQUENCE)
-    private Integer eventid;
+    private Integer id;
 
     @Column(nullable = false)
     private String date;
