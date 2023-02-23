0e681ae34573720a06d769e7726c394193e8cfd7
NAWINRAJM <94778263+NAWINRAJM@users.noreply.github.com>
2023-02-23 14:01:39 +0545

    Update discuss.java

--- src/main/java/com/example/security/educare/Pojo/discuss.java
+++ src/main/java/com/example/security/educare/Pojo/discuss.java
@@ -1,23 +1,25 @@
-package com.system.springboot4.pojo;
-import com.system.springboot4.entity.Discuss;
-import com.system.springboot4.entity.User;
+package com.example.security.educare.Pojo;
+import com.example.security.educare.Entity.Discuss;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-// Pojo backend ko validation ko laagi ekdamai important
+
 
 @Getter
 @Setter
-@NoArgsConstructor // dynamic banaunw duitai pass gareko
+@NoArgsConstructor
 @AllArgsConstructor
 
 public class discuss {
     private int id;
     private String message;
+    private String name1;
     public discuss(Discuss discuss1){
         this.id=discuss1.getId();
         this.message=discuss1.getMessage();
+        this.name1=discuss1.getName1();
+
     }
 }
